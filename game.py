77c86dfa64a278f969446aababe3b08e533e773e
Jon Eier <dodde174@gmail.com>
2022-09-01 14:17:47 +0200

    0.2

--- game.py
+++ game.py
@@ -6,8 +6,8 @@ screen = pygame.display.set_mode((800,450))
 pygame.display.set_caption("Jons spill")
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((200,100))
-test_surface.fill("Yellow")
+box = pygame.Rect(100, 100, 20, 20)
+movementSpeed = 4
 
 while True:
   for event in pygame.event.get():
@@ -15,7 +15,19 @@ while True:
       pygame.quit()
       exit()
 
-  screen.blit(test_surface,(200,100))
+  keys = pygame.key.get_pressed()
+
+  if keys[pygame.K_w]:
+    box.move_ip(0, -movementSpeed)
+  if keys[pygame.K_a]:
+    box.move_ip(-movementSpeed, 0)
+  if keys[pygame.K_s]:
+    box.move_ip(0, movementSpeed)
+  if keys[pygame.K_d]:
+    box.move_ip(movementSpeed, 0)
+
+  screen.fill("black")
+  pygame.draw.rect(screen, "yellow", box)
 
   pygame.display.update()
   clock.tick(60)
