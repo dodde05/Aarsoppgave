bc2262129367f2ceae8301f637420554b44d7525
Jon Eier <dodde174@gmail.com>
2022-09-06 14:38:33 +0200

    0.6

--- game.py
+++ game.py
@@ -1,16 +1,19 @@
 import pygame
 from sys import exit
-import level
+import map
 
 class Game:
   def __init__(self):
-
+    
     pygame.init()
-    self.screen = pygame.display.set_mode((level.WIDTH, level.HEIGHT))
+    self.screen = pygame.display.set_mode((800, 450))
     self.clock = pygame.time.Clock()
+
+    self.player = Player()
 
   def run(self):
     while True:
+
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           pygame.quit()
@@ -28,8 +31,10 @@ class Game:
         player.box.move_ip(player.movementSpeed, 0)
 
       self.screen.fill("black")
-      pygame.draw.rect(self.screen, "yellow", player.box)
 
+      terrain.create_level()
+      pygame.draw.rect(self.screen, "yellow", player.box)
+      
       pygame.display.update()
       self.clock.tick(60)
 
@@ -39,8 +44,22 @@ class Player:
     self.box = pygame.Rect(100, 100, 20, 20)
     self.movementSpeed = 4
 
+class Terrain:
+  def __init__(self):
+    self.rects = []
+
+    for row_i, row in enumerate(map.map_layout):
+      for col_i, col in enumerate(row):
+        if col == "o":
+          self.rects.append(pygame.Rect(col_i * map.TILESIZE, row_i * map.TILESIZE, map.TILESIZE, map.TILESIZE))
+
+  def create_level(self):
+    for i, rect in enumerate(self.rects):
+      pygame.draw.rect(game.screen, "red", self.rects[i])
+
 if __name__ == "__main__":
   game = Game()
   player = Player()
+  terrain = Terrain()
 
   game.run()
--- map.py
+++ map.py
@@ -1,10 +1,7 @@
-WIDTH = 800
-HEIGHT = 450
-FPS = 60
 TILESIZE = 25
 
 #32*18 25(800*450)
-level_layout = [
+map_layout = [
 "",
 "",
 "",
